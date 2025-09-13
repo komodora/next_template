90b9004f8d96666bd1fb42d1b9a4ae68e168f6c0
komodora <724komodora915@gmail.com>
2025-09-13 10:33:55 +0000

    chore: eslintのv9対応

--- next-template/eslint.config.mjs
+++ next-template/eslint.config.mjs
@@ -1,16 +1,26 @@
-import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
+  baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ['eslint:recommended', 'next'],
-  }),
-];
+const eslintConfig = defineConfig([
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+]);
 
 export default eslintConfig;
